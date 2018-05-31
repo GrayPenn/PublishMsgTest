9a3064767d8553f6dca57764b35449330cb2613a
oldboyooxx <pengguiyang1@126.com>
2018-05-31 13:43:49 +0800

    测试

---
+++ src/Test.java
@@ -0,0 +1,27 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        String msg = "aaa";
+        System.out.println(Integer.parseInt("666"));
+        Date d1 = new Date();
+        Date d2 = new Date();
+        System.out.println(d1.after(d2));
+        System.out.println(new Date(2018,05,29).after(d2));
+        System.out.println(d1);
+        System.out.println(  Calendar.getInstance().getTime());
+        Calendar c = Calendar.getInstance();
+        c.set(2018,05,29);
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        //System.out.println(c.compareTo(Calendar.getInstance().getTime()));
+        d1 = c.getTime();
+        System.out.println(d2);
+        System.out.println(d1);
+        System.out.println(d1.compareTo(d2));
+        int b = 5;
+        double a =5.5;
+        double d = b+a;
+        System.out.println(d);
+    }
+}
